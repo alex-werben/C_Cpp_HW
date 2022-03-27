750c28bd34c1e1ba4eac34266f726dcd8ef28c92
alexwerben <crystall.werben@gmail.com>
2022-03-27 15:33:50 +0300

    ci cpplint add

--- linters/run.sh
+++ linters/run.sh
@@ -21,9 +21,9 @@ function check_log() {
     fi
 }
 
-# print_header "RUN cppcheck"
-# check_log "cppcheck **/*.c IT--enable=all --inconclusive --error-exitcode=1 -I add_composition -I view_composition -I string_io --suppress=missingIncludeSystem" "\(information\)"
-# print_header "SUCCESS cppcheck"
+print_header "RUN cppcheck"
+check_log "cppcheck **/*.c IT--enable=all --inconclusive --error-exitcode=1 -I add_composition -I view_composition -I string_io --suppress=missingIncludeSystem" "\(information\)"
+print_header "SUCCESS cppcheck"
 
 print_header "RUN clang-tidy"
 check_log "clang-tidy **/*.c -warnings-as-errors=* -extra-arg=-std=c99 -- -Iadd_composition -Iview_composition -Istring_io" "Error (?:reading|while processing)"
