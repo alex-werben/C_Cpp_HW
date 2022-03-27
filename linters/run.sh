0d4aa88d3e1972da59fa60cd5be9cbee8cef8081
alexwerben <crystall.werben@gmail.com>
2022-03-27 15:11:04 +0300

    ci changed

--- linters/run.sh
+++ linters/run.sh
@@ -21,13 +21,13 @@ function check_log() {
     fi
 }
 
-print_header "RUN cppcheck"
-check_log "cppcheck **/*.c IT--enable=all --inconclusive --error-exitcode=1 -I add_composition -I view_composition -I string_io --suppress=missingIncludeSystem" "\(information\)"
-print_header "SUCCESS cppcheck"
+# print_header "RUN cppcheck"
+# check_log "cppcheck **/*.c IT--enable=all --inconclusive --error-exitcode=1 -I add_composition -I view_composition -I string_io --suppress=missingIncludeSystem" "\(information\)"
+# print_header "SUCCESS cppcheck"
 
-# print_header "RUN clang-tidy"
-# check_log "clang-tidy **/*.c -warnings-as-errors=* -extra-arg=-std=c99 -- -Iadd_composition -Iview_composition -Istring_io" "Error (?:reading|while processing)"
-# print_header "SUCCESS clang-tidy"
+print_header "RUN clang-tidy"
+check_log "clang-tidy **/*.c -warnings-as-errors=* -extra-arg=-std=c99 -- -Iadd_composition -Iview_composition -Istring_io" "Error (?:reading|while processing)"
+print_header "SUCCESS clang-tidy"
 
 # print_header "RUN cpplint"
 # check_log "cpplint --extensions=c **/*.c" "Can't open for reading"
