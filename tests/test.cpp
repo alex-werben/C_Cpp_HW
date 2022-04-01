8c2380ef2e95391967fa33a7e329aa8107cd7c9d
alexwerben <crystall.werben@gmail.com>
2022-04-01 21:48:03 +0300

    Test changes

--- tests/test.cpp
+++ tests/test.cpp
@@ -6,12 +6,13 @@ extern "C" {
 }
 
 TEST (Interface, test_consistent_alg) {
-  char str[] = "EECCAABBDEEDDDCCDEE";
-  char* arr = (char*)malloc(20 * sizeof(char));
-  for (int i = 0; i < 20; ++i) {
-    arr[i] = str[i];
-  }
-  consistent_algorithm(arr);
+  // char str[] = "EECCAABBDEEDDDCCDEE";
+  // char* arr = (char*)malloc(20 * sizeof(char));
+  // for (int i = 0; i < 20; ++i) {
+  //   arr[i] = str[i];
+  // }
+  // consistent_algorithm(arr);
+  call_consistent_alg();
   // EXPECT_EQ(call_consistent_alg(), 2);
   EXPECT_EQ(sequence_generator(20, 3, 2), 2);
   // free(arr);
