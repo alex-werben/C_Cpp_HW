4b1f619a8c67aaf534ceb80f0dd50a7f29ea6045
alexwerben <crystall.werben@gmail.com>
2022-04-01 15:33:57 +0300

    Interface changed

--- tests/test.cpp
+++ tests/test.cpp
@@ -6,10 +6,6 @@ extern "C" {
 }
 
 TEST (Interface, test_consistent_alg) {
-  
-  EXPECT_EQ(sequence_generator(20, 3, 2), call_consistent_alg());
+  sequence_generator(20, 3, 2);
+  EXPECT_EQ(2, call_consistent_alg());
 }
-
-// TEST (Interface, test_length_series) {
-//   EXPECT_EQ(length_series(2), 2);
-// }
