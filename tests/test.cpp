fed5757e81b3e544b6260c0b0c8570ca29f2a062
alexwerben <crystall.werben@gmail.com>
2022-04-01 21:37:49 +0300

    CI configuration changed

--- tests/test.cpp
+++ tests/test.cpp
@@ -7,5 +7,7 @@ extern "C" {
 
 TEST (Interface, test_consistent_alg) {
   sequence_generator(20, 3, 2);
+  char arr[] = "EECCAABBDEEDDDCCDEE";
+  consistent_algorithm(arr);
   // EXPECT_EQ(call_consistent_alg(), 2);
 }
