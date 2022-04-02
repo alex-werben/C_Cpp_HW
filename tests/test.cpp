732c376e9224bc85c59787a29d8f7fea5f9a8481
alexwerben <crystall.werben@gmail.com>
2022-04-02 03:21:12 +0300

    Test for consistent algorithm

--- tests/test.cpp
+++ tests/test.cpp
@@ -33,7 +33,7 @@ TEST (Interface, test_interface) {
   // EXPECT_EQ(call_sequence_generator(fp), 1);
 
   EXPECT_EQ(sequence_generator(20, 3, 2), 2);
-  // EXPECT_EQ(call_consistent_alg(), 2);
+  EXPECT_EQ(call_consistent_alg(), 2);
   // fclose(fp);
 }
 
